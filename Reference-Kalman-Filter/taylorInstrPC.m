7552367e651224face8780b281fd5444543ad2e1
Axel Karlsson <axeka624@student.liu.se>
2025-04-23 16:07:22 +0200

    init commit reference filter

---
+++ Reference-Kalman-Filter/taylorInstrPC.m
@@ -0,0 +1,18 @@
+function [g, G] = taylorInstrPC(pl, at, o, oInd, tc, x)
+nInstr = length(at);
+g = zeros(nInstr,1);
+G = zeros(nInstr,length(x));
+
+for j=1:nInstr
+  ind = oInd(j):(oInd(j+1)-1);
+  if (at(j) == pl.atIBOR)
+    eox = exp(-o(ind,:)*x);
+    g(j) = (eox-1)/tc(ind);
+    G(j,:) = -eox*o(ind,:)/tc(ind);
+  elseif (at(j) == pl.atOISG)
+    eox = exp(o(ind,:)*x);
+    den = tc(ind(2:end))'*eox(2:end);
+    g(j) = (eox(1)-eox(end))/den;
+    G(j,:) = (eox(1)*o(ind(1),:)-eox(end)*o(ind(end),:))/den - (eox(1)-eox(end))/den^2*((tc(ind(2:end)).*eox(2:end))'*o(ind(2:end),:));
+  end
+end
