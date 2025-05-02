04b1149016c7a52d747b607e2ee0d9fe56658927
InsertCompanyNameHerePlease <carl.jungerth@gmail.com>
2025-05-02 13:22:06 +0200

    Clean formatting main.jl

--- Efficient-Kalman-Filter/main.jl
+++ Efficient-Kalman-Filter/main.jl
@@ -17,34 +17,57 @@ using .pricingFunctions
 # Load full data
 data = loadData.run()
 
-# Split data: p% in-sample: (1-p)% out-of-sample
+# Split data: p% in-sample, (1-p)% out-of-sample
 p = 0.8
-split = loadData.split_data(data,p)
-
+split = loadData.split_data(data, p)
 data_insample = split.insample
 data_outsample = split.outsample
 
 # Run Filter
-x_filt, P_filt, x_smooth, P_smooth, P_lag, oAll, EAll = simpleEKF.kalman_filter_smoother_lag1(data_insample.zAll, data_insample.oIndAll, data_insample.tcAll, data_insample.I_z_t, data_insample.f_t, Int(data_insample.n_c), Int(data_insample.n_p), Int(data_insample.n_s), Int(data_insample.n_t), Int(data_insample.n_u), Int(data_insample.n_x), Int.(data_insample.n_z_t), data_insample.A_t, data_insample.B_t, data_insample.D_t, data_insample.G_t, data_insample.Sigma_w, data_insample.Sigma_v, vec(data_insample.a_x), data_insample.Sigma_x, vec(data_insample.theta_F), data_insample.theta_g, data_insample.firstDates, data_insample.tradeDates, data_insample.ecbRatechangeDates, data_insample.T0All, data_insample.TAll);
+x_filt, P_filt, x_smooth, P_smooth, P_lag, oAll, EAll = simpleEKF.kalman_filter_smoother_lag1(
+    data_insample.zAll,
+    data_insample.oIndAll,
+    data_insample.tcAll,
+    data_insample.I_z_t,
+    data_insample.f_t,
+    Int(data_insample.n_c),
+    Int(data_insample.n_p),
+    Int(data_insample.n_s),
+    Int(data_insample.n_t),
+    Int(data_insample.n_u),
+    Int(data_insample.n_x),
+    Int.(data_insample.n_z_t),
+    data_insample.A_t,
+    data_insample.B_t,
+    data_insample.D_t,
+    data_insample.G_t,
+    data_insample.Sigma_w,
+    data_insample.Sigma_v,
+    vec(data_insample.a_x),
+    data_insample.Sigma_x,
+    vec(data_insample.theta_F),
+    data_insample.theta_g,
+    data_insample.firstDates,
+    data_insample.tradeDates,
+    data_insample.ecbRatechangeDates,
+    data_insample.T0All,
+    data_insample.TAll
+)
 
 # Calculate Forward Rates and Repricing
 fAll, priceAll = outputData.calculateRateAndRepricing(
-    EAll, data_insample.zAll, data_insample.I_z_t, x_smooth, oAll, data_insample.oIndAll, data_insample.tcAll, data_insample.theta_g, Int.(data_insample.n_z_t), Int(data_insample.n_t), Int(data_insample.n_s)
+    EAll,
+    data_insample.zAll,
+    data_insample.I_z_t,
+    x_smooth,
+    oAll,
+    data_insample.oIndAll,
+    data_insample.tcAll,
+    data_insample.theta_g,
+    Int.(data_insample.n_z_t),
+    Int(data_insample.n_t),
+    Int(data_insample.n_s)
 )
 
 # Plot Forward Rate Curve (Should be done in Matlab instead)
 plots.plot3DCurve(data_insample.times, fAll)
-
-
-
-
-# function dummy_nll(ψ)
-#     x, y = ψ
-#     return x^2 + 2y^2
-# end
-
-# ψ₀ = [4.0, -5.0]
-# ψ_opt = newtonMethod.newtonOptimize(dummy_nll, ψ₀; verbose=true)
-
-# println("\nOptimized ψ = ", ψ_opt)
-
